57f65590b06814bef1aa69a7cc730558e8234196
Mshwadfi <mohamedalshwadfy24@gmail.com>
2025-01-10 18:42:09 +0200

    resolve errors in categories params

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
+"use client"
 import CategoryFilter from "./_components/CategoryFilter";
 import PropertyList from "./_components/PropertyList";
+import { useState } from "react"; // Import useState to manage selectedCategory
 
+export default function Home() {
+  const [selectedCategory, setSelectedCategory] = useState<string>("All");
 
+  const handleSelectCategory = (category: string) => {
+    setSelectedCategory(category);
+  };
 
-export default async function Home() {
   return (
     <>
       <div className="container mx-auto flex flex-wrap justify-start gap-6 px-8 sm:px-14">
-      <CategoryFilter />
+        <CategoryFilter
+          selectedCategory={selectedCategory}
+          onSelectCategory={handleSelectCategory}
+        />
         {/* <ImageCarousel images={images} />
         <ImageCarousel images={images} />
         <ImageCarousel images={images} />
