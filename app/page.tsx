54a7dedc3d45370b78ff08c920d51da5f9ac678f
Mshwadfi <mohamedalshwadfy24@gmail.com>
2025-01-12 18:45:17 +0200

    prepare for build

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,9 @@ export default async function Home() {
   return (
     <>
       <div className="container mx-auto flex flex-wrap justify-start gap-6 px-8 sm:px-14">
-      <CategoryFilter />
+      <CategoryFilter selectedCategory={""} onSelectCategory={function (category: string): void {
+          throw new Error("Function not implemented.");
+        } } />
         <PropertyList />
       </div>
     </>
